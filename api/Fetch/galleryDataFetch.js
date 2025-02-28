9461044979cb9cb6d55cfc4b7e4ff11f072e9d4b
simpletools101 <simpletools101@hotmail.com>
2025-02-28 13:21:08 +0300

    criks

--- api/Fetch/galleryDataFetch.js
+++ api/Fetch/galleryDataFetch.js
@@ -10,7 +10,7 @@ class GalleryDataFetchManager {
   initFetch() {
     fetchGalleryPageData().then((data) => {
       let __cdata = data.newLifeGalleryPages;
-      console.log("GALLERY-DATA",__cdata)
+      console.log("GALLERY-DATA", __cdata);
       __cdata[0].image.forEach((item) => {
         let derivedItem = this.galleryItemTemplate(item.url);
         this.parentContainer.insertAdjacentHTML("afterbegin", derivedItem);
@@ -20,29 +20,10 @@ class GalleryDataFetchManager {
 
   galleryItemTemplate(imageURL) {
     let template = `
-
-            <li class="filterall filter-gallery filter-events filter-worship eg-gallery-wrapper">
-                <!-- THE CONTAINER FOR THE MEDIA AND THE COVER EFFECTS -->
-                <div class="esg-media-cover-wrapper">
-                    <!-- THE MEDIA OF THE ENTRY -->
-                    <div class="esg-entry-media">
-                        <img src="${imageURL}" alt="">
-                    </div>
-                    <!-- THE CONTENT OF THE ENTRY -->
-                    <div class="esg-entry-cover esg-fade" data-delay="0">
-                        <!-- THE COLORED OVERLAY -->
-                        <div class="esg-overlay esg-fade eg-gallery-container" data-delay="0"></div>
-                        <div class="esg-center eg-gallery-element-0-a esg-falldown" data-delay="0.1">
-                            <a class="eg-gallery-element-0 esgbox" href="#">
-                                                                    
-                                <i class="eg-icon-plus"></i>
-                            </a>
-                        </div>
-                        <div class="esg-center eg-gallery-element-8 esg-none esg-clear"></div>
-                    </div>
-                    <!-- END OF THE CONTENT IN THE ENTRY -->
-                </div>
-                <!-- END OF THE CONTAINER FOR THE MEDIA AND COVER/HOVER EFFECTS -->
+    <li>
+            <div class="gallery-picture-item">
+              <img src="${imageURL}" />
+            </div>
             </li>
         
         `;
